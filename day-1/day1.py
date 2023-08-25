d00ad3b0a5b42f9e059bc09cf208b18efbf4e7a9
hardikyadav12 <hardik.ind12@gmail.com>
2023-08-25 17:23:02 +0530

    assignment-2

--- day-1/day1.py
+++ day-1/day1.py
@@ -1,3 +1,8 @@
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
+
+
+print("Hello World\nHello World")
+
+print("Hello " + "Hardik")
